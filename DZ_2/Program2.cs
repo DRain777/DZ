1e9256ce510333a2233c3f971678367aa0645334
Denis <d.cochew@yandex.ru>
2022-09-23 22:54:53 +0700

    dz_2

---
+++ DZ_2/Program2.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу которая на входе принимает 2 числа и выдает,
+// какочисло больше какое меньше.
+Console.WriteLine("Первое число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("второе число ");
+int b = Convert.ToInt32(Console.ReadLine());
+if(a > b)
+{
+    Console.WriteLine("Максимальное число: " +  a );
+}
+else 
+{
+     Console.WriteLine("Максимальное число: " +  b );
+}
+
+
