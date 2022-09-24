937cf70746f4bc06229c7dc6265e3ae9d079ae5f
Denis <d.cochew@yandex.ru>
2022-09-24 17:19:06 +0700

    DZ_4

---
+++ DZ_3/DZ_4/Program4.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу которая на вход примет число и выдас является оно четным или нет
+Console.WriteLine("Хочешь узнать четное ли число");
+Console.WriteLine("Твое число");
+int a = Convert.ToInt32(Console.ReadLine());
+if( a % 2 == 0)
+{
+    Console.WriteLine("Твое число четное");
+}
+else
+{
+    Console.WriteLine(" Нечетное");
+}
