1a6b9b028a776a02e46c5b1f1f913939026ef737
Denis <d.cochew@yandex.ru>
2022-10-05 00:00:24 +0700

    Domzadachi

--- Zadachi/Zadachi.cs
+++ Zadachi/Zadachi.cs
@@ -91,13 +91,19 @@ void DomZadacha_0()
 // значное число и на выходе показывает вторую цифру этого числа
 Console.WriteLine(" Введите трехзначное число");
 int numb = Convert.ToInt32(Console.ReadLine());
-int number = numb % 100 / 10 ;
-Console.Write(number);
+if(numb > 99 && numb < 999)
+{
+   Console.WriteLine(" Вторая цифра" +" " + numb % 100 / 10); // /10%/10
+}
+else
+{
+    Console.WriteLine(" ЧИСЛО НЕ 3Х ЗНАЧНОЕ");
+}   
 }
 void DomZadacha_1()
 {
-//Домашняя работа.Напишите программу, которая принимает на вход цифру, обозначающую день недели,
-//, и проверяет, является ли этот день выходным.
+// Домашняя работа.Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+// , и проверяет, является ли этот день выходным.
 Console.WriteLine("Введите число от 1 до 7 обозначающий день недели");
 int day = Convert.ToInt32(Console.ReadLine());
 if( day == 6 || day == 7)
@@ -109,15 +115,223 @@ else
     Console.WriteLine(day + " Будни");
 }
 }
-//Домашнее задание Напишите программу, которая с помощью деления выводит третью цифру 
-//или сообщает, что третьей цифры нет.645->5 78- третьей цифры нет 3267432979 -> 6
+void DomZadacha_2()
+{
+// Домашнее задание Напишите программу, которая с помощью деления выводит третью цифру 
+// или сообщает, что третьей цифры нет.645->5 78- третьей цифры нет 3267432979 -> 6
 Console.WriteLine(" Введите  число");
 int number = Convert.ToInt32(Console.ReadLine());
-int n = number % 10;// Спомощью деления из 645 нашел число 5
- Console.WriteLine(n);
-// Как показать что 3 цифры нет с помощью програмирования я пока не понял Обьясните на уроке пожалуста
-// Есть догадки что надо сравнивать переменную с > 2 но как это записать пока не знаю.
+number = Math.Abs(number);
+if(number > 99)
+{
+    while(number >999)
+{
+    number /= 10; 
+}    
+ Console.WriteLine(" Третья Цифра" + "  " + number % 10 );
+}
+else
+{
+    Console.WriteLine( " Третьей цифры нет");
+}    
+}
+// DateTime lastTime = DateTime.Now;
+// int number = 123;
+// for(int i = 0; i > 1000000000; i++)
+// {
+//     var n = number/10% 10;    // Convert.ToString(number)[1];
+// } 
+// Console.WriteLine(DateTime.Now - lastTime);
+//void Zadach_massiv()
+// Домашнее задание.Напишит программу которая принимает на вход трех 
+// значное число и на выходе показывает вторую цифру этого числа
+// Console.WriteLine("Введите число от 1 до 7 обозначающий день недели");
+// int day = Convert.ToInt32(Console.ReadLine());
+// string[] week = {"пн","вт","ср","чт","пт","сб","вс"};
+// if(day > 0 && day < 8)
+// {
+//    Console.WriteLine(week[day-1]);
+// if (day == 6 || day == 7)
+// {
+//     Console.WriteLine("Выходной");
+// }
+// else
+// {
+//     Console.WriteLine(" Будни");
+// }
+// }
+void Zadacha17()
+{
+    // напишите программу,которая принимает на вход координаты точки (x и y),причем x не равно 0 и Y не равно 0 и выдает номер
+    // четверти плоскости.в которой находится
+    Random random = new Random();
+    int x = random.Next(-10,11);
+    int y = random.Next(-10,11);
+    Console.WriteLine($"a{x}, {y})");
+    if( x > 0 && y > 0)
+{
+    Console.WriteLine("Точка лежит в первой четверти");
+}
+else if ( x > 0 && y > 0)
+{
+    Console.WriteLine("Точка лежит во второй четверти");
+}
+else if( x < 0 && y > 0)
+{
+     Console.WriteLine("Точка лежит в первой четверти");
+}
+else if( x > 0 && y < 0)
+{
+     Console.WriteLine("Точка лежит в первой четверти");
+}
+else
+{
+    Console.WriteLine("Точка лежит на одной из осей");
+}    
+}
+void Zadacha18()
+{
+//Задача 18: Напишите программу, которая по
+//заданному номеру четверти, показывает диапазон
+//возможных координат точек в этой четверти (x и y)
+Console.WriteLine("Введите номер четверти ");
+int numb = Convert.ToInt32(Console.ReadLine());
+if(numb == 1)
+{
+    Console.WriteLine( " Введенная четверть > o и y > o");
+}
+else if(numb == 2)
+{
+    Console.WriteLine("Введенная четверть x < 0 и Y > 0"); 
+}
+else if(numb == 3)
+{
+    Console.WriteLine("Введенная четвертьx x < 0 и y < 0" );
+}
+else if (numb == 4)
+{
+    Console.WriteLine(" Введенная четверть x > 0 и Y < 0");
+}
+else
+{
+    Console.WriteLine("Вы вели не коректную четверть");
+}
+}
+ void Zadacha21()
+ {
+//Задача 21: Напишите программу, которая принимает на вход координаты двух точек и
+//находит расстояние между ними в 2Dпространстве.
+//A (3,6); B (2,1) -> 5,09
+//A (7,-5); B (1,-1) -> 7,21
+Random random = new Random();
+int ax = random.Next(-10,11);
+int ay = random.Next(-10,11);
+int bx = random.Next(-10,11);
+int by = random.Next(-10,11);
 
-
-
-
+Console.WriteLine($"Точки: А {ax},{ay},B {bx},{by}");
+Console.WriteLine($" Растояния между точками А и Б:{Math.Sqrt(Math.Pow((bx - ax),2) + Math.Pow((by - ay),2))}" );
+ }
+ void Zadacha22()
+ {
+//Задача 22: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу
+//квадратов чисел от 1 до N
+int number = 17;
+int counter = 1;
+while(counter <= number)
+{
+    Console.WriteLine($"{counter} * {counter} = {counter * counter} ");
+    counter++;
+}
+ }
+ void Zadacha19_domashka()
+ {
+// Задача 19 Напишите программу, которая принимает на вход пятизначное
+//  число и проверяет, является ли оно палиндромом.
+int number = 18381;
+int result = 0;
+int current_number = number;
+if(number < 9999 | number > 99999)
+{
+    Console.WriteLine($"число {number} не пяти значное ");
+}
+while (current_number > 0)
+{ 
+    int digit = current_number  % 10;
+    result = result * 10 + digit; 
+    current_number /= 10;
+    Console.WriteLine(result);
+}
+if(number == result)
+{
+    Console.WriteLine($"{number} Палиндром");
+}
+else
+{
+    Console.WriteLine($"{number} не палиндром");
+}
+}
+void Zadacha19_domashka_0()
+{
+//Задача 19 Напишите программу, которая принимает на вход пятизначное
+//  число и проверяет, является ли оно палиндромом.
+Console.WriteLine(" Введите 5-ти значное число");
+int number = Convert.ToInt32(Console.ReadLine());
+string numbstring = Convert.ToString(number); 
+if(number < 9999 | number > 99999)
+{
+    Console.WriteLine($"число {number} не 5-ти значное ");
+}
+else
+{
+    if(numbstring[4] == numbstring[0] && numbstring[3] == numbstring[1] )
+    {
+        Console.WriteLine($"{number} является Палиндромом");
+    }
+    else
+    {
+        Console.WriteLine($"{number} НЕ является Палиндромом");   
+    }
+}
+}   
+void DomZadacha_21()
+{
+//Задача 21 Напишите программу, которая принимает на вход координаты 
+//двух точек и находит расстояние между ними в 3D пространстве.
+        //A (3,6,8); B (2,1,-7), -> 15.84
+        //A (7,-5, 0); B (1,-1,9) -> 11.53
+int a_x = 7;
+int a_y = 7;
+int a_z = 7;
+int b_x = 0;
+int b_y = 0;
+int b_z = 0; 
+double result = Math.Sqrt(Math.Pow(a_x - b_x,2) + Math.Pow(a_y - b_y,2) + Math.Pow(a_z - b_z,2));
+Console.Write($"{result} Растояния");
+}
+ void Zadacha_Dom23()
+ {
+// Задача 23 Напишите программу, которая принимает на вход 
+// число (N) и выдаёт таблицу кубов чисел от 1 до N.                    
+//  3 -> 1, 8, 27   5 -> 1, 8, 27, 64, 125
+Console.WriteLine("Введите число N ");
+int n = Convert.ToInt16(Console.ReadLine());
+int count = 1;
+while (count <= n)
+{
+    Console.WriteLine($"{count} * {count} * {count} = { count * count * count}");
+    count++;
+}
+}
+ void Zadacha_Dom23for()
+ {
+// Задача 23 Напишите программу, которая принимает на вход 
+// число (N) и выдаёт таблицу кубов чисел от 1 до N.  Через for                   
+//  3 -> 1, 8, 27   5 -> 1, 8, 27, 64, 125
+Console.WriteLine("Введите число N ");
+int n = Convert.ToInt16(Console.ReadLine());
+for(int i = 1; i <= n; i++)
+{
+    Console.WriteLine($"{i} * {i} * {i} = {Math.Pow(i, 3)}");
+}
+}
